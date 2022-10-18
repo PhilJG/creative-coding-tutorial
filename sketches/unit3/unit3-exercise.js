dff85d128ef75baf361ae4b1975793572274339f
Phil <phl.j.gray@gmail.com>
2022-10-18 13:44:36 -0700

    two eyes added to unit3-excercise

--- sketches/unit3/unit3-exercise.js
+++ sketches/unit3/unit3-exercise.js
@@ -28,20 +28,23 @@ const sketch = () => {
 
     context.fillStyle = 'black';
     
-    const cx = width * 0.5;
-    const cy = height * 0.5
-
-    const w = width * 0.01
-    const h = height * 0.1;
-
-    let x,y;
-
-    const num = random.range(500 , 2000);
-    const radius = width * 0.3;
+    
     //outer pupil
     context.save();
-    //set location of circle
-    context.translate(540, 540);
+    //pw/ph = pupil width & height location. 
+    // rw/rh = rectangle width and height location
+    function drawPupil(pw, ph, rw, rh){
+      let x,y;
+      
+      const num = random.range(500 , 2000);
+      const radius = width * 0.3;
+      
+      const cx = width * rw;
+      const cy = height * rh
+  
+      const w = width * 0.01
+      const h = height * 0.1;
+      context.translate(pw, ph);
 
     // draw circle
     context.beginPath();
@@ -57,14 +60,14 @@ const sketch = () => {
       const slice = degToRad(360 / num);
       const angle = slice * i;
  
-       x = cx + radius * Math.sin(angle)
-       y = cy + radius * Math.cos(angle)
- 
+      x = cx + radius * Math.sin(angle)
+      y = cy + radius * Math.cos(angle)
+      
        context.save(); 
        context.translate(x, y);
        context.rotate(-angle);
        context.scale(random.range(0.2, 1), random.range(0.2, 1));
-   
+       
        //Trigonometery: relationship between sides and angles on triangles
        //https://ramesaliyev.com/trigonoparty/
        
@@ -74,29 +77,37 @@ const sketch = () => {
        context.fillStyle = getRandomColor()
        context.fill();
        context.restore();
- 
+       
        context.save();
        context.translate(cx, cy);
        context.rotate(-angle);
        
        context.lineWidth = random.range(5,20);
- 
+       
        context.beginPath();
        context.arc(0,  0, radius * random.range(0.1, 1.3), slice * random.range(1, -8), slice * random.range(1,5));
        context.strokeStyle = getRandomColor();
        context.stroke();
        context.restore();
-     }
-    //inner pupil
-  context.save();
-    //set location of circle
-    context.translate(540, 540);
-    // draw circle
-    context.beginPath();
-    context.arc(0,0, random.range(50, 190), 0, Math.PI * 2);
-    context.fill();
-    context.restore();
-
+      }
+      //inner pupil
+      context.save();
+      //set location of circle
+      context.translate(pw, ph);
+      // draw circle
+      context.beginPath();
+      context.arc(0,0, random.range(50, 190), 0, Math.PI * 2);
+      context.fill();
+      context.restore();
+      
+          }
+          //(pw, ph, rw, rh)
+    drawPupil(0, 540, 0, .5)
+    drawPupil(1080, 540, 1, .5) 
+ 
+     
+       
+    
 
   };
 };
